da5181ce6b62130d4f6ab3b256642a873be9e294
Huajieyu001 <331173500@qq.com>
2026-01-30 21:12:02 +0800

    虚假唤醒

--- l1-juc/src/main/java/top/huajieyu001/chatper004/WaitNotifyTest.java
+++ l1-juc/src/main/java/top/huajieyu001/chatper004/WaitNotifyTest.java
@@ -17,8 +17,10 @@ public class WaitNotifyTest {
 
     private static boolean hasCigarette = false;
 
+    private static boolean hasTakeout = false;
+
     public static void main(String[] args) {
-        waitNotify();
+        waitNotifyMultiNotifyWhere();
     }
 
     public static void noWaitNotify() {
@@ -114,18 +116,28 @@ public class WaitNotifyTest {
                 }
                 log.debug("Has Cigarette: {}", hasCigarette);
                 if(hasCigarette){
-                    log.debug("Working");
+                    log.debug("Cigarette Working");
                 }
             }
         }, "Worker").start();
 
-        for (int i = 0; i < 5; i++) {
-            new Thread(() -> {
-                synchronized (lock) {
-                    log.debug("Working");
+        new Thread(() -> {
+            synchronized (lock) {
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(!hasTakeout){
+                    log.debug("Resting");
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }, "Else worker").start();
-        }
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(hasTakeout){
+                    log.debug("hasTakeout Working");
+                }
+            }
+        }, "Worker").start();
 
         try {
             TimeUnit.SECONDS.sleep(1);
@@ -136,7 +148,112 @@ public class WaitNotifyTest {
         new Thread(() -> {
             synchronized (lock) {
                 hasCigarette = true;
+                hasTakeout = true;
                 lock.notify();
+            }
+        }, "Gift").start();
+    }
+
+    public static void waitNotifyMultiNotifyAll() {
+        new Thread(() -> {
+            synchronized (lock) {
+                log.debug("Has Cigarette: {}", hasCigarette);
+                if(!hasCigarette){
+                    log.debug("Resting");
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                log.debug("Has Cigarette: {}", hasCigarette);
+                if(hasCigarette){
+                    log.debug("Cigarette Working");
+                }
+            }
+        }, "Worker").start();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(!hasTakeout){
+                    log.debug("Resting");
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(hasTakeout){
+                    log.debug("hasTakeout Working");
+                }
+            }
+        }, "Worker").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            synchronized (lock) {
+                hasCigarette = true;
+                hasTakeout = true;
+                lock.notifyAll();
+            }
+        }, "Gift").start();
+    }
+
+    public static void waitNotifyMultiNotifyWhere() {
+        new Thread(() -> {
+            synchronized (lock) {
+                log.debug("Has Cigarette: {}", hasCigarette);
+                while(!hasCigarette){
+                    log.debug("Resting");
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                log.debug("Has Cigarette: {}", hasCigarette);
+                if(hasCigarette){
+                    log.debug("Cigarette Working");
+                }
+            }
+        }, "Worker111").start();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(!hasTakeout){
+                    log.debug("Resting");
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                log.debug("Has Takeout: {}", hasTakeout);
+                if(hasTakeout){
+                    log.debug("hasTakeout Working");
+                }
+            }
+        }, "Worker222").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            synchronized (lock) {
+                hasTakeout = true;
+                lock.notifyAll();
+                hasCigarette = true;
             }
         }, "Gift").start();
     }
