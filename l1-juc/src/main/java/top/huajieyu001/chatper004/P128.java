5ca410a65e62b16467538ddc74e031d78619daa0
Huajieyu001 <331173500@qq.com>
2026-02-01 16:46:40 +0800

    使用wait/notify或者park/unpark控制线程执行顺序

--- l1-juc/src/main/java/top/huajieyu001/chatper004/P128.java
+++ l1-juc/src/main/java/top/huajieyu001/chatper004/P128.java
@@ -2,6 +2,8 @@ package top.huajieyu001.chatper004;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @Author huajieyu
  * @Date 2026/2/1 15:56
@@ -16,6 +18,13 @@ public class P128 {
     static boolean flag = false;
 
     public static void main(String[] args) {
+        test3();
+    }
+
+    /**
+     * 用Thread.join控制执行顺序
+     */
+    public static void test1() {
         Thread t1 = new Thread(() -> {
             log.debug("A");
         });
@@ -41,5 +50,63 @@ public class P128 {
         t3.start();
         t2.start();
         t1.start();
+    }
+
+    /**
+     * 用wait notify控制执行顺序
+     */
+    public static void test2() {
+        P128 t = new P128();
+
+        new Thread(() -> {
+            synchronized (t) {
+                while (!flag) {
+                    try {
+                        t.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (flag) {
+                        log.debug("A");
+                        break;
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (t) {
+                log.debug("B");
+                flag = true;
+                t.notify();
+            }
+        }).start();
+    }
+
+    /**
+     * 用park，unpark控制执行顺序
+     */
+    public static void test3() {
+        P128 t = new P128();
+
+        Thread t1 = new Thread(() -> {
+            LockSupport.park();
+            log.debug("A");
+        });
+
+        Thread t2 = new Thread(() -> {
+            LockSupport.park();
+            log.debug("B");
+            LockSupport.unpark(t1);
+        });
+
+        Thread t3 = new Thread(() -> {
+            log.debug("C");
+            LockSupport.unpark(t2);
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
