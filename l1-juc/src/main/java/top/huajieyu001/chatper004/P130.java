7bdcfa88c7c58f8e2583f8e5e6a722e1a6ce669c
Huajieyu001 <331173500@qq.com>
2026-02-03 22:06:56 +0800

    三个线程，分别打印a，b，c，依次交替打印abc5次，使用wait+notify和共享变量来实现

--- l1-juc/src/main/java/top/huajieyu001/chatper004/P130.java
+++ l1-juc/src/main/java/top/huajieyu001/chatper004/P130.java
@@ -22,7 +22,10 @@ public class P130 {
     static int status = 1;
 
     public static void main(String[] args) {
+        test3();
+    }
 
+    public static void test1() {
         Thread t1 = new Thread(() -> {
             synchronized (lock) {
                 while (true) {
@@ -95,5 +98,136 @@ public class P130 {
         t1.start();
         t2.start();
         t3.start();
+    }
+
+    public static void test2() {
+        Thread t1 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+                    try {
+                        if (status != 1){
+                            lock.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (status == 1) {
+                        status = 2;
+                        countA--;
+                        log.debug("a");
+                        lock.notifyAll();
+                        if (countA == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+                    try {
+                        if (status != 2){
+                            lock.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (status == 2) {
+                        status = 3;
+                        countB--;
+                        log.debug("b");
+                        lock.notifyAll();
+                        if (countB == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread t3 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+                    try {
+                        if (status != 3){
+                            lock.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (status == 3) {
+                        status = 1;
+                        countC--;
+                        log.debug("c");
+                        lock.notifyAll();
+                        if (countC == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public static void test3() {
+        WaitNotifyLoop obj = new WaitNotifyLoop(5);
+
+        new Thread(() -> {
+            try {
+                obj.print("a", 1, 2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                obj.print("b", 2, 3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                obj.print("c", 3, 1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+}
+
+class WaitNotifyLoop {
+
+    private int status = 1;
+
+    private int loopCount;
+
+    public WaitNotifyLoop(int loopCount) {
+        this.loopCount = loopCount;
+    }
+
+    public int getLoopCount() {
+        return loopCount;
+    }
+
+    public void print(String str, int waitFlag, int nextFlag) throws InterruptedException {
+        for (int i = 0; i < loopCount; i++) {
+            synchronized (this) {
+                while (status != waitFlag) {
+                    this.wait();
+                }
+                System.out.print(str);
+                status = nextFlag;
+                this.notifyAll();
+            }
+        }
     }
 }
